0a6e1d5c6e457eb19203b26c7b4137f291314175
Nikolas Haug <nikohaug21@gmail.com>
2021-02-11 11:16:48 -0600

    clean up code

--- gatsby/src/pages/blog.js
+++ gatsby/src/pages/blog.js
@@ -6,20 +6,15 @@ export default function Blog({ data }) {
     const posts = data.allPostsJson.edges;
 
     const [list, setList] = useState([...posts.slice(0, 10)]);
-    console.log(list);
     const [loadMore, setLoadMore] = useState(false);
     const [hasMore, setHasMore] = useState(posts.length > 10);
-
     const [extendedList, setExtendedList] = useState(list.length);
-    console.log(extendedList);
 
     useEffect(() => {
         if(loadMore && hasMore) {
             const currentLength = list.length;
             const isMore = currentLength < posts.length;
             const nextResults = isMore ? posts.slice(currentLength, currentLength + 10) : [];
-            console.log('currentLength', currentLength);
-            console.log('nextResults', nextResults);
             setExtendedList(currentLength);
             setList([...list, ...nextResults]); 
             setLoadMore(false);
@@ -37,24 +32,13 @@ export default function Blog({ data }) {
         setLoadMore(true)
     }
 
-    // create an array for delay times
-    const makeDelayArray = () => {
-        let delayArr = [];
-        for(let i = 0; i < 10; i++) {
-            delayArr.push(i);
-        }
-        return delayArr;
-    }
-
-    console.log(makeDelayArray());
-
     return (
         <>
             <h1>blog</h1>
             <div className="row">
                 {
                     list.map((post, i) => (
-                        <BlogPost key={post.node.id} post={post.node} delay={list.length == 10 ? i : i - extendedList} />
+                        <BlogPost key={post.node.id} post={post.node} delay={list.length == extendedList ? i : i - extendedList} />
                     ))
                 }
             </div>
