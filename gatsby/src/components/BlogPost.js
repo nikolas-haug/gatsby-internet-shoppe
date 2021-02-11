fc4685c11a00a9612e3fce304c107a6340b07e67
Nikolas Haug <nikohaug21@gmail.com>
2021-02-10 21:33:21 -0600

    add basic grid animation

--- gatsby/src/components/BlogPost.js
+++ gatsby/src/components/BlogPost.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function BlogPost({ post }) {
+export default function BlogPost({ post, delay }) {
     return (
         <>
-            <div className="blogpost col-med-4">
+            <div className="blogpost col-med-4" style={{ animationDelay: `${delay * 100}ms` }}>
                 <img src={post.url} alt="" className="img-100" />
                 <p>{post.title}</p>
             </div>
--- gatsby/src/pages/blog.js
+++ gatsby/src/pages/blog.js
@@ -1,19 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { graphql } from 'gatsby';
 import BlogPost from '../components/BlogPost';
 
 export default function Blog({ data }) {
     const posts = data.allPostsJson.edges;
-    console.log(posts);
+
+    const [list, setList] = useState([...posts.slice(0, 10)]);
+    console.log(list);
+    const [loadMore, setLoadMore] = useState(false);
+    const [hasMore, setHasMore] = useState(posts.length > 10);
+
+    useEffect(() => {
+        if(loadMore && hasMore) {
+            const currentLength = list.length;
+            const isMore = currentLength < posts.length;
+            const nextResults = isMore ? posts.slice(currentLength, currentLength + 10) : [];
+            setList([...list, ...nextResults]); 
+            setLoadMore(false);
+        }
+    }, [loadMore, hasMore]); // eslint-disable-line
+
+    //Check if there is more
+    useEffect(() => {
+        const isMore = list.length < posts.length;
+        setHasMore(isMore);
+    }, [list]); //eslint-disable-line
+
+    // Load more button click
+    const handleLoadMore = () => {
+        setLoadMore(true)
+    }
+
     return (
         <>
             <h1>blog</h1>
             <div className="row">
                 {
-                    posts.map((post) => (
-                        <BlogPost key={post.node.id} post={post.node} />
+                    list.map((post, i) => (
+                        <BlogPost key={post.node.id} post={post.node} delay={i} />
                     ))
                 }
+            </div>
+            <div className="row justify-content-center margin-top margin-bottom">
+                {hasMore ? (
+                    <button onClick={handleLoadMore}>Load More</button>
+                    ) : (
+                    <p>No more results</p>
+                )}
             </div>
         </>
     )
