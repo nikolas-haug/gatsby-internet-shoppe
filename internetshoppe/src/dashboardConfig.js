4a30be1fa2812dfe821c6ef8adc7921517e3701f
Nikolas Haug <nikohaug21@gmail.com>
2021-03-08 15:53:08 -0600

    add dashboard structure menu widget

--- internetshoppe/src/dashboardConfig.js
+++ internetshoppe/src/dashboardConfig.js
@@ -1,7 +1,23 @@
 export default {
     widgets: [
-        { name: 'project-info', layout: { width: 'auto' } },
-        { name: 'project-users' },
-        { name: 'cats' }
+        // { name: 'project-info', layout: { width: 'auto' } },
+        // { name: 'project-users' },
+        { name: 'structure-menu' },
+        {
+            name: 'document-list',
+            options: {
+                title: 'Last edited products',
+                order: '_updatedAt desc',
+                types: ['product']
+            }
+        },
+        {
+            name: 'document-list',
+            options: {
+                title: 'Last edited documents',
+                order: '_updatedAt desc'
+            }
+        }
+
     ]
 }
